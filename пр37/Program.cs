6768b534eefc0c29a22335b247dbaf42b554b158
yankabelosludtseva <nblsldcv@gmail.com>
2026-05-01 16:34:23 +0500

    Разработанная структура проекта. Белослудцева

--- пр37/Program.cs
+++ пр37/Program.cs
@@ -1,6 +1,9 @@
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddMvc(option => option.EnableEndpointRouting = false);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
-
+app.UseDeveloperExceptionPage();
+app.UseStatusCodePages();
+app.UseStaticFiles();
+app.UseMvcWithDefaultRoute();
 app.Run();
