b2965e8cdfabe70a6ad3be4a5d4df40cd70371e9
yankabelosludtseva <nblsldcv@gmail.com>
2026-05-02 22:36:15 +0500

    Получение данных о предметах. Белослудцева

---
+++ Shop_Belosludtseva/Data/DataBase/DBItems.cs
@@ -0,0 +1,38 @@
+﻿using MySql.Data.MySqlClient;
+using Shop_Belosludtseva.Data.Common;
+using Shop_Belosludtseva.Data.Interfaces;
+using Shop_Belosludtseva.Data.Models;
+
+namespace Shop_Belosludtseva.Data.DataBase
+{
+    public class DBItems : IItems
+    {
+        public IEnumerable<Categories> Categories = new DBCategory().AllCategories;
+
+        public IEnumerable<Items> AllItems
+        {
+            get
+            {
+                List<Items> items = new List<Items>();
+                MySqlConnection MySqlConnection = Connection.MySqlOpen();
+                MySqlDataReader ItemsData = Connection.MySqlQuery("SELECT * FROM Shop.items ORDER BY `Name`;", MySqlConnection);
+
+                while (ItemsData.Read())
+                {
+                    items.Add(new Items()  // исправлено: new Items)
+                    {
+                        Id = ItemsData.IsDBNull(0) ? -1 : ItemsData.GetInt32(0),
+                        Name = ItemsData.IsDBNull(1) ? "" : ItemsData.GetString(1),
+                        Description = ItemsData.IsDBNull(2) ? "" : ItemsData.GetString(2),
+                        Img = ItemsData.IsDBNull(3) ? "" : ItemsData.GetString(3),
+                        Price = ItemsData.IsDBNull(4) ? -1 : ItemsData.GetInt32(4),
+                        Category = ItemsData.IsDBNull(5) ? null : Categories.FirstOrDefault(x => x.Id == ItemsData.GetInt32(5))
+                    });
+                }
+
+                Connection.MySqlClose(MySqlConnection);
+                return items;
+            }
+        }
+    }
+}
