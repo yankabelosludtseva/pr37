3132d3c2b7329e981713703be1f0054a114a1042
yankabelosludtseva <nblsldcv@gmail.com>
2026-05-02 22:35:12 +0500

    Получение данных о категориях. Белослудцева

---
+++ Shop_Belosludtseva/Data/DataBase/DBCategory.cs
@@ -0,0 +1,33 @@
+﻿using MySql.Data.MySqlClient;
+using Shop_Belosludtseva.Data.Common;
+using Shop_Belosludtseva.Data.Interfaces;
+using Shop_Belosludtseva.Data.Models;
+
+namespace Shop_Belosludtseva.Data.DataBase
+{
+    public class DBCategory : ICategorys
+    {
+       public IEnumerable<Categories> AllCategories
+        {
+            get
+            {
+                List<Categories> categories = new List<Categories>();
+                MySqlConnection MySqlConnection = Connection.MySqlOpen();
+                MySqlDataReader CategorysData = Connection.MySqlQuery("SELECT * FROM Shop.Categorys ORDER BY `Name`;", MySqlConnection);
+
+                while (CategorysData.Read())
+                {
+                    categories.Add(new Categories()
+                    {
+                        Id = CategorysData.IsDBNull(0) ? -1 : CategorysData.GetInt32(0),
+                        Name = CategorysData.IsDBNull(1) ? null : CategorysData.GetString(1),
+                        Description = CategorysData.IsDBNull(2) ? null : CategorysData.GetString(2)
+                    });
+                }
+
+                Connection.MySqlClose(MySqlConnection); // добавлено закрытие соединения
+                return categories;
+            }
+        }
+    }
+}
