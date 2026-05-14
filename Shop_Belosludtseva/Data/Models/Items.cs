a62921bf168e50a0633977a5f866cfa7aef94020
yankabelosludtseva <nblsldcv@gmail.com>
2026-05-14 16:23:57 +0500

    Доработка JS скриптов для уменьшения и увеличения предметов в корзине. Белослуцдева

--- Shop_Belosludtseva/Data/Models/Items.cs
+++ Shop_Belosludtseva/Data/Models/Items.cs
@@ -14,5 +14,18 @@
         public int Price { get; set; }
         /// <summary> Категория товара
         public Categories Category { get; set; }
+
+        public Items(Items item = null)
+        {
+            if (item != null)
+            {
+                this.Id = item.Id;
+                this.Name = item.Name;
+                this.Description = item.Description;
+                this.Img = item.Img;
+                this.Price = item.Price;
+                this.Category = item.Category;
+            }
+        }
     }
 }
---
+++ Shop_Belosludtseva/Data/Models/ItemsBasket.cs
@@ -0,0 +1,11 @@
+﻿namespace Shop_Belosludtseva.Data.Models
+{
+    public class ItemsBasket : Items
+    {
+        public int Count {  get; set; }
+        public ItemsBasket(int Count, Items item) : base(item) 
+        {
+            this.Count = Count;
+        }
+    }
+}
